bf4e936d293c5b4af68690d41fa15aceeac83692
vinicius-pimenta <viniciustavarespimenta.2020@gmail.com>
2020-05-08 18:54:14 -0300

    :rocket: Masterclass #1

---
+++ note.md
@@ -0,0 +1,8 @@
+# React JS Curso Rápido ⚛️ Masterclass #1 2020 - 100% Prático!
+
+-  [Masterclass #1](https://www.youtube.com/watch?v=XQxitgyZ_S4&t=2226s)
+
+```bash
+$ npx create-react-app fundamentos-react
+$ npm start
+```
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+import React from 'react'
+
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+import Primeiro from './components/Primeiro'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComIf'
+
+export default props => (
+  <div className="App">
+    <Card titulo="#06 - Condicional v2">
+      <CondicionalComIf numero={10}></CondicionalComIf>
+    </Card>
+    <Card titulo="#05 - Condicional v1">
+      <Condicional numero={11}></Condicional>
+    </Card>
+    <Card titulo="#04 - Repetição">
+      <Repeticao></Repeticao>
+    </Card>
+    <Card titulo="#03 - Componente Com Filhos">
+      <ComFilhos>
+        <ul>
+          <li>Ana</li>
+          <li>Bia</li>
+          <li>Carlos</li>
+          <li>Daniel</li>
+        </ul>
+      </ComFilhos>
+    </Card>
+    <Card titulo="#02 - Componente Com Parametro">
+      <ComParametro titulo="Esse é o título" subtitulo="Esse é o subtítulo" />
+    </Card>
+    <Card titulo="#01 - Primeiro Componente">
+      <Primeiro />
+    </Card>
+
+    {/*  */}
+    {/* <Primeiro />
+    <ComParametro titulo="Opa" subtitulo="Epa" /> */}
+  </div>
+);
